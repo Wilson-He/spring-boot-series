430292492b22117a6372398b740cf6f430185845
ex_hewx <ex_hewx@partner.midea.com>
2019-05-09 09:52:09 +0800

    add validation

---
+++ spring-boot-validation-demo/src/main/java/io/github/validation/ValidationApp.java
@@ -0,0 +1,14 @@
+package io.github.validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+public class ValidationApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ValidationApp.class);
+    }
+}
---
+++ spring-boot-validation-demo/src/main/java/io/github/validation/controller/UserBaseController.java
@@ -0,0 +1,34 @@
+package io.github.validation.controller;
+
+import io.github.validation.group.InsertGroup;
+import io.github.validation.group.UpdateGroup;
+import io.github.validation.vo.UserVO;
+import io.swagger.annotations.Api;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * <p>
+ * 前端控制器
+ * </p>
+ *
+ * @author Wilson
+ * @since 2019-04-22
+ */
+@RestController
+@RequestMapping("/userBase")
+@Api
+public class UserBaseController {
+
+
+    @PostMapping("/")
+    public UserVO add(@RequestBody @Validated(InsertGroup.class) UserVO vo) {
+        return vo;
+    }
+
+    @PutMapping("/")
+    public UserVO update(@RequestBody @Validated(UpdateGroup.class) UserVO vo) {
+        return vo;
+    }
+}
+
