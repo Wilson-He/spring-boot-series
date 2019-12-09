370c527eda1f5e8092165fcea8b5e18a87d21955
ex_hewx <ex_hewx@partner.midea.com>
2019-12-09 10:57:42 +0800

    添加配置中心

--- spring-cloud-config-central/src/main/java/io/cloud/config/ConfigServerApplication.java
+++ spring-cloud-config-central/src/main/java/io/cloud/config/ConfigServerApplication.java
@@ -1,8 +1,12 @@
 package io.cloud.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+import javax.annotation.PostConstruct;
 
 /**
  * @author Wilson
@@ -10,7 +14,16 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  **/
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableConfigServer
 public class ConfigServerApplication {
+    @Value("${rocketmq.topic}")
+    private String topic;
+
+    @PostConstruct
+    public void init() {
+        System.out.println("topic:" + topic);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
     }
