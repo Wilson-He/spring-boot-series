a981c4fe7cd2c805a29d591beb90a6a143b49cbf
ex_hewx <ex_hewx@partner.midea.com>
2020-01-02 10:41:08 +0800

    add docker

--- spring-boot-nginx-docker-demo/docker-run.sh
+++ spring-boot-nginx-docker-demo/docker-run.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
 
 # docker-spring-boot
-docker run --rm  -h docker-spring-boot -d \
-     -p 8080:8080 \
+docker run -h docker-spring-boot -d \
+     -p 8999:8080 \
      -v /etc/localtime:/etc/localtime:ro\
      -v docker-spring-boot-docker:/app \
      --name docker-spring-boot docker-spring-boot/latest
+#     -e LC_ALL="en_US.UTF-8" -e TZ="Asia/Shanghai" \
 
 # docker-spring-boot-2
 docker run --rm  -h docker-spring-boot-2 -d \
      -p 8080:8080 \
-     - /etc/timezone:/etc/timezone:ro \
+     -v /etc/timezone:/etc/timezone:ro \
      -v /etc/localtime:/etc/localtime:ro \
      -v docker-spring-boot-docker-2:/app \
      --name docker-spring-boot-2 docker-spring-boot/latest
 
-docker run --rm -d -h  docker-spring-boot- -p 80:80 \
+docker run --rm -d -h  docker-spring-boot-2 -p 80:80 \
         -v /etc/localtime:/etc/localtime \
         -v nginx:/etc/nginx/ \
-        --link docker-spring-boot-:docker-spring-boot- --name ecs-nginx  nginx
+        --name ecs-nginx  nginx
 
 # Nginx双SpringBoot容器负载均衡
 docker run --rm -d -h  ecs-nginx -p 80:80 \
         -v /etc/localtime:/etc/localtime \
         -v nginx:/etc/nginx/ \
         --link docker-spring-boot --link docker-spring-boot-2 --name ecs-nginx  nginx
+
+
+docker run -d \
+         -v /etc/localtime:/etc/localtime:ro\
+         -v docker-spring-boot:/app \
+         --name docker-spring-boot docker-spring-boot/latest
