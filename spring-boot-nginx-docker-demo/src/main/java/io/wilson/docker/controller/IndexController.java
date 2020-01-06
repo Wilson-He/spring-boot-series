ad6710c031a1c6bbeaade41afadfd939a1575903
ex_hewx <ex_hewx@partner.midea.com>
2020-01-06 11:08:32 +0800

    1

--- spring-boot-nginx-docker-demo/src/main/java/io/wilson/docker/controller/IndexController.java
+++ spring-boot-nginx-docker-demo/src/main/java/io/wilson/docker/controller/IndexController.java
@@ -30,8 +30,4 @@ public class IndexController {
         return ServerResponse.success("info");
     }
 
-    @GetMapping("/hello")
-    public ServerResponse hello() {
-        return ServerResponse.success("hello world");
-    }
 }
