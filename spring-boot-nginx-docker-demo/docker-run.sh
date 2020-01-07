9662994bb0a297506f68a94eb650b313cf816300
ex_hewx <ex_hewx@partner.midea.com>
2020-01-07 10:25:58 +0800

    1

--- spring-boot-nginx-docker-demo/docker-run.sh
+++ spring-boot-nginx-docker-demo/docker-run.sh
@@ -2,9 +2,7 @@
 
 # docker-spring-boot
 docker run --rm -h docker-spring-boot -d \
-     -p 8080:8080 \
      -v /etc/localtime:/etc/localtime:ro\
-     -v /var/run/docker.sock:/var/run/docker.sock \
      --name docker-spring-boot docker-spring-boot/latest
      
 docker run --rm  -h docker-spring-boot -d -p 8080:8080 \
@@ -18,7 +16,6 @@ docker run -d \
 
 # docker-spring-boot-2
 docker run --rm  -h docker-spring-boot-2 -d \
-     -p 8080:8080 \
      -v /etc/timezone:/etc/timezone:ro \
      -v docker-spring-boot-docker-2:/app \
      --name docker-spring-boot-2 docker-spring-boot/latest
@@ -34,7 +31,8 @@ docker run -d -h docker-nginx -p 80:80 \
 docker run --rm -d -h  ecs-nginx -p 80:80 \
         -v /etc/localtime:/etc/localtime \
         -v nginx:/etc/nginx/ \
-        --link docker-spring-boot --link docker-spring-boot-2 \
+        --link docker-spring-boot \
+        --link docker-spring-boot-2 \
         --name ecs-nginx  nginx
 
 
--- spring-boot-nginx-docker-demo/startup.sh
+++ spring-boot-nginx-docker-demo/startup.sh
@@ -5,11 +5,24 @@ mvn clean package
 if [ -e "./volumes/app/docker-spring-boot.jar" ]
   then rm -f ./volumes/app/docker-spring-boot.jar \
         && cp ./target/docker-spring-boot.jar ./volumes/app/docker-spring-boot.jar \
-		&& docker-compose -p docker-spring-boot up -d \
 		&& docker restart docker-spring-boot \
+		# nginx容器重启
+        # && docker restart docker-nginx \
         && echo "update restart success"
   else mkdir volumes/app -p \
         && cp ./target/docker-spring-boot.jar ./volumes/app/docker-spring-boot.jar \
 		&& docker-compose -p docker-spring-boot up -d \
         && echo "first start"
 fi
+#
+#if [ -e "./volumes/app/docker-spring-boot.jar" ]
+#  then rm -f ./volumes/app/docker-spring-boot.jar \
+#        && cp ./target/docker-spring-boot.jar ./volumes/app/docker-spring-boot.jar \
+#		&& docker restart docker-spring-boot \
+#		&& docker restart docker-nginx \
+#        && echo "update restart success"
+#  else mkdir volumes/app -p \
+#        && cp ./target/docker-spring-boot.jar ./volumes/app/docker-spring-boot.jar \
+#		&& docker-compose -p docker-spring-boot up -d \
+#        && echo "first start"
+#fi
