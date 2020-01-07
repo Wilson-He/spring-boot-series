b9416b0ed2f6f14f2c0ba75414a3b58338b20dfb
ex_hewx <ex_hewx@partner.midea.com>
2020-01-07 10:26:38 +0800

    1

--- spring-boot-nginx-docker-demo/startup.sh
+++ spring-boot-nginx-docker-demo/startup.sh
@@ -1,28 +1,14 @@
 #!/usr/bin
-BUILD_ID=DONTKILLME
+# BUILD_ID=DONTKILLME
 cd /var/lib/jenkins/workspace/docker-spring-boot/spring-boot-nginx-docker-demo
 mvn clean package
 if [ -e "./volumes/app/docker-spring-boot.jar" ]
   then rm -f ./volumes/app/docker-spring-boot.jar \
         && cp ./target/docker-spring-boot.jar ./volumes/app/docker-spring-boot.jar \
 		&& docker restart docker-spring-boot \
-		# nginx容器重启
-        # && docker restart docker-nginx \
         && echo "update restart success"
   else mkdir volumes/app -p \
         && cp ./target/docker-spring-boot.jar ./volumes/app/docker-spring-boot.jar \
 		&& docker-compose -p docker-spring-boot up -d \
         && echo "first start"
 fi
-#
-#if [ -e "./volumes/app/docker-spring-boot.jar" ]
-#  then rm -f ./volumes/app/docker-spring-boot.jar \
-#        && cp ./target/docker-spring-boot.jar ./volumes/app/docker-spring-boot.jar \
-#		&& docker restart docker-spring-boot \
-#		&& docker restart docker-nginx \
-#        && echo "update restart success"
-#  else mkdir volumes/app -p \
-#        && cp ./target/docker-spring-boot.jar ./volumes/app/docker-spring-boot.jar \
-#		&& docker-compose -p docker-spring-boot up -d \
-#        && echo "first start"
-#fi
