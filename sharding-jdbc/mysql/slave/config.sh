efbb91214710a06a5d4595a2ce25760e942461cf
ex_hewx <ex_hewx@partner.midea.com>
2020-01-15 11:20:10 +0800

    1

--- sharding-jdbc/mysql/slave/config.sh
+++ sharding-jdbc/mysql/slave/config.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-change master to master_host='mysql-master1', master_user='slave', master_password='123456', master_port=3306, master_log_file='master1-bin.000003', master_log_pos=154, master_connect_retry=30;
+change master to master_host='mysql-master1', master_user='slave', master_password='123456', master_port=3306, master_log_file='master1-bin.000004', master_log_pos=154, master_connect_retry=30;
+change master to master_host='111.229.4.194', master_user='slave', master_password='123456', master_port=3306, master_log_file='master1-bin.000004', master_log_pos=154, master_connect_retry=30;
