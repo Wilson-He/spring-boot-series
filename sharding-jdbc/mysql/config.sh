eac66a54b865d0eee8b4e8e79d6bba06249ca118
ex_hewx <ex_hewx@partner.midea.com>
2020-01-16 10:58:00 +0800

    1

---
+++ sharding-jdbc/mysql/config.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+change master to master_host='mysql-master1', master_user='slave', master_password='123456', master_port=3306, master_log_file='master1-bin.000005', master_log_pos=154, master_connect_retry=30;
