11b82c2844c5d3546e36b6ef88c506d6b635ec24
ex_hewx <ex_hewx@partner.midea.com>
2020-01-16 11:30:07 +0800

    1

--- sharding-jdbc/mysql/config.sh
+++ sharding-jdbc/mysql/config.sh
@@ -9,4 +9,4 @@ docker exec -it mysql-master2-slave1 /bin/bash
 
 
 change master to master_host='mysql-master1', master_user='slave', master_password='123456', master_port=3306, master_log_file='master1-bin.000003', master_log_pos=154, master_connect_retry=30;
-change master to master_host='mysql-master2', master_user='slave', master_password='123456', master_port=4306, master_log_file='master1-bin.000004', master_log_pos=154, master_connect_retry=30;
+change master to master_host='mysql-master2', master_user='slave', master_password='123456', master_port=4306, master_log_file='master1-bin.000005', master_log_pos=154, master_connect_retry=30;
